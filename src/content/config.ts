b38de140012f0f90b474c4a6de06776c1e3b80d1
Mario Yepes <marioy47@gmail.com>
2023-10-23 15:47:49 -0500

    feat: removing Gastsby scripts

---
+++ src/content/config.ts
@@ -0,0 +1,17 @@
+import { defineCollection, z } from 'astro:content';
+
+const posts = defineCollection({
+  type: 'content',
+  schema: ({image}) => {
+    return z.object({
+        title: z.string(),
+        date: z.coerce.date(),
+        update: z.coerce.date().optional(),
+        tags: z.string(),
+        cover: image().optional(),
+        draft: z.boolean().optional().default(false)
+      })
+  } 
+})
+
+export const collections = { posts };

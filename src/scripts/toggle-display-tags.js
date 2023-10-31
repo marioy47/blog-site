c6092b881f844ee1a36e05ffcf9b5cd70b9afd4e
Mario Yepes <marioy47@gmail.com>
2023-10-30 22:35:36 -0500

    feat: adding filtering option to the projects

---
+++ src/scripts/toggle-display-tags.js
@@ -0,0 +1,25 @@
+
+function toggleDisplayArticle(tags) {
+    Object.keys(tags).forEach(article => {
+      const selector = `[data-project-id=${article}]`;
+      if (tags[article].find(element => `#${element }` == location.hash)) {
+        document.querySelector(selector).removeAttribute('style');
+        console.log('showing', selector);
+      } else {
+        console.log('removing', selector);
+        document.querySelector(selector).setAttribute('style', 'display: none');
+      } 
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const tags = {};
+  const [...articles ] = document.querySelectorAll('article')
+  articles.forEach(article => {
+    tags[article.dataset.projectId] = article.dataset.tags.split(',');
+  });
+  if (location.hash) {
+    toggleDisplayArticle(tags);
+  }
+  window.addEventListener('hashchange', () => toggleDisplayArticle(tags));
+})

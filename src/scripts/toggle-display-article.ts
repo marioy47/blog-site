c9ae23a63acb076f29de29926d5185abd38efd89
Mario Yepes <marioy47@gmail.com>
2023-11-07 16:22:32 -0500

    fix: issue with tag filtering after adding transitions

---
+++ src/scripts/toggle-display-article.ts
@@ -0,0 +1,34 @@
+
+const toggleDisplayArticle = (tags, filter) => {
+    Object.keys(tags).forEach(article => {
+      const selector = `[data-article-id=${article}]`;
+      if (tags[article].find(element => element === filter)) {
+        document.querySelector(selector).removeAttribute('style');
+      } else {
+        document.querySelector(selector).setAttribute('style', 'display: none');
+      } 
+    });
+}
+
+const getTagsInDatasets: Object<string|array> = ( selector ) => {
+  const tags = {};
+  const [...articles ] = document.querySelectorAll(selector)
+  articles.forEach(article => {
+    tags[article.dataset.articleId] = article.dataset.tags.split(',');
+  });
+  return tags;
+}
+
+const tags = getTagsInDatasets('article');
+
+document.addEventListener('astro:page-load', () => {
+  if (location.hash) {
+    toggleDisplayArticle(tags, location.hash.replace('#',''));
+  }
+  //Unfortunately windows.addEventListener('hashchange') does not work on transtions.
+  [...document.querySelectorAll('ul>li>a')].forEach(link => {
+    const u = new URL(link.getAttribute('href'), 'https://whatever.com');
+    link.addEventListener('click', ()=> toggleDisplayArticle(tags, u.hash.replace('#','')));
+  })
+});
+

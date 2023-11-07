83ee30c3983ab9aef460bbb789eeb653959d0045
Mario Yepes <marioy47@gmail.com>
2023-11-07 16:22:32 -0500

    fix: issue with tag filtering after adding transitions

---
+++ src/scripts/layout-site-footer.ts
@@ -0,0 +1,9 @@
+
+document.addEventListener('astro:page-load', () => {
+  const scrollToTop = document.getElementById('scroll-to-top');
+  if ( scrollToTop ) {
+    scrollToTop.addEventListener('click', () => {
+      window.scrollTo(0, 0);
+    });
+  }
+});

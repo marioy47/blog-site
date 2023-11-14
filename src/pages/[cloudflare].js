2e311023239a6f9de93cd8181970391b8c281a17
Mario Yepes <marioy47@gmail.com>
2023-11-14 15:53:46 -0500

    feat: adding portfolio redirects and 404 page

--- src/pages/[cloudflare].js
+++ src/pages/[cloudflare].js
@@ -3,10 +3,15 @@ import {getCollection} from 'astro:content';
 export async function GET({params}) {
   if (params?.cloudflare === '_redirects') {
     const posts = await getCollection('posts');
-    const redirects = posts.reduce((accum , current) => {
+    const redirPosts = posts.reduce((accum , current) => {
       return `${accum}/${current.slug} /blog/${current.slug} 301\n`;
     },'');
-    return new Response(redirects);
+
+    const projects = await getCollection('projects');
+    const redirProjects = projects.reduce((accum , current) => {
+      return `${accum}/${current.slug} /portfolio/${current.slug} 301\n`;
+    },'');
+    return new Response(redirPosts + redirProjects);
   }
 }
 

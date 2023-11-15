a6cef454b965d50a6f93956954cd6c9c4f6acefa
Mario Yepes <marioy47@gmail.com>
2023-11-14 22:22:46 -0500

    feat: adding the partytown extension to speedup analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import remarkToc from 'remark-toc';
 import sitemap from '@astrojs/sitemap';
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://marioyepes.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://marioyepes.com',
+  integrations: [mdx(), sitemap(), partytown()],
   markdown: {
     remarkPlugins: [remarkToc],
     shikiConfig: {

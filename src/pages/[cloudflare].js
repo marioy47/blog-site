71ef5a0e1605d115f9039592c69bdfe83307744f
Mario Yepes <marioy47@gmail.com>
2023-11-15 10:57:46 -0500

    fix: fixing redirects by adding trailing slash

--- src/pages/[cloudflare].js
+++ src/pages/[cloudflare].js
@@ -4,12 +4,12 @@ export async function GET({params}) {
   if (params?.cloudflare === '_redirects') {
     const posts = await getCollection('posts');
     const redirPosts = posts.reduce((accum , current) => {
-      return `${accum}/${current.slug} /blog/${current.slug} 301\n`;
+      return `${accum}/${current.slug}/ /blog/${current.slug}/ 301\n`;
     },'');
 
     const projects = await getCollection('projects');
     const redirProjects = projects.reduce((accum , current) => {
-      return `${accum}/${current.slug} /portfolio/${current.slug} 301\n`;
+      return `${accum}/${current.slug}/ /portfolio/${current.slug}/ 301\n`;
     },'');
     return new Response(redirPosts + redirProjects);
   }
